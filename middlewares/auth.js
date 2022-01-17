14983452792f441a324c49a931eac18dbbd0f6a9
manuelfrancisco <franciscomanueljose@gmail.com>
2022-01-17 08:23:54 -0300

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,20 @@
 const isAdmin = async (req, res, next) => {
-  throw new Error('Not implemented');
+  // not implemented
+  next();
 };
 
 const isAuth = async (req, res, next) => {
-  throw new Error('Not implemented');
+  // not implemented
+  next();
+};
+
+const isOwnUser = async (req, res, next) => {
+  // not implemented
+  next();
 };
 
 module.exports = {
   isAdmin,
   isAuth,
+  isOwnUser
 };
