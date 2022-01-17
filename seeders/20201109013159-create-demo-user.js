7d02edd75214ab0fb38599d75a521edee5d51b78
manuelfrancisco <franciscomanueljose@gmail.com>
2022-01-17 09:18:50 -0300

    Update 20201109013159-create-demo-user.js

--- seeders/20201109013159-create-demo-user.js
+++ seeders/20201109013159-create-demo-user.js
@@ -3,133 +3,21 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Users', [{
-      firstName: 'Agustin',
-      lastName: 'Tafura',
-      email: 'agustin_tafura@test.com',
-      // Important: Password not encrypted yet! 
-      password: '$2b$10$oA9KaOX81D.szm.wG0zPo.QsFIDj0PiYYRwBbGDsL285YZbavAQSK',
+      firstName: 'Standard',
+      lastName: 'Test User',
+      email: 'standard-test-user@test.com',
+      password: '$2a$12$pnRk4pz/CjzV10l.p0RkoujnS1Z104wq3ILF8dKY.SioVC0f1qnzy', //TestPassword123!
       roleId: 1,
-      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      image: 'https://i.pinimg.com/280x280_RS/2e/45/66/2e4566fd829bcf9eb11ccdb5f252b02f.jpg',
       createdAt: new Date,
       updatedAt: new Date
     },{
-      firstName: 'Andrea',
-      lastName: 'Maccan',
-      email: 'andrea_maccan@test.com',
-      // Important: Password not encrypted yet! 
-      password: '$2b$10$oA9KaOX81D.szm.wG0zPo.QsFIDj0PiYYRwBbGDsL285YZbavAQSK',
-      roleId: 1,
-      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
-      createdAt: new Date,
-      updatedAt: new Date
-    },{
-      firstName: 'Ezequiel',
-      lastName: 'Astrada',
-      email: 'ezequiel_astrada@test.com',
-      // Important: Password not encrypted yet! 
-      password: '$2b$10$oA9KaOX81D.szm.wG0zPo.QsFIDj0PiYYRwBbGDsL285YZbavAQSK',
-      roleId: 1,
-      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
-      createdAt: new Date,
-      updatedAt: new Date
-    },{
-      firstName: 'Franco',
-      lastName: 'Garancini',
-      email: 'franco_garancini@test.com',
-      // Important: Password not encrypted yet! 
-      password: '$2b$10$oA9KaOX81D.szm.wG0zPo.QsFIDj0PiYYRwBbGDsL285YZbavAQSK',
-      roleId: 1,
-      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
-      createdAt: new Date,
-      updatedAt: new Date
-    },{
-      firstName: 'Joaquin',
-      lastName: 'Bascur',
-      email: 'bascur@test.com',
-      // Important: Password not encrypted yet! 
-      password: '$2b$10$oA9KaOX81D.szm.wG0zPo.QsFIDj0PiYYRwBbGDsL285YZbavAQSK',
-      roleId: 1,
-      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
-      createdAt: new Date,
-      updatedAt: new Date
-    },{
-      firstName: 'Juan Pablo',
-      lastName: 'Choter',
-      email: 'juantablo_ch@test.com',
-      // Important: Password not encrypted yet! 
-      password: '$2b$10$oA9KaOX81D.szm.wG0zPo.QsFIDj0PiYYRwBbGDsL285YZbavAQSK',
-      roleId: 1,
-      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
-      createdAt: new Date,
-      updatedAt: new Date
-    },{
-      firstName: 'Matias',
-      lastName: 'Dalceggio',
-      email: 'matias_dalceggio@test.com',
-      // Important: Password not encrypted yet! 
-      password: '$2b$10$oA9KaOX81D.szm.wG0zPo.QsFIDj0PiYYRwBbGDsL285YZbavAQSK',
-      roleId: 1,
-      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
-      createdAt: new Date,
-      updatedAt: new Date
-    },{
-      firstName: 'Sebastian',
-      lastName: 'Galvan',
-      email: 'seba_galvan@test.com',
-      // Important: Password not encrypted yet! 
-      password: '$2b$10$oA9KaOX81D.szm.wG0zPo.QsFIDj0PiYYRwBbGDsL285YZbavAQSK',
-      roleId: 1,
-      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
-      createdAt: new Date,
-      updatedAt: new Date
-    },{
-      firstName: 'Manuel',
-      lastName: 'Francisco',
-      email: 'manu_f@test.com',
-      // Important: Password not encrypted yet! 
-      password: '$2b$10$oA9KaOX81D.szm.wG0zPo.QsFIDj0PiYYRwBbGDsL285YZbavAQSK',
-      roleId: 1,
-      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
-      createdAt: new Date,
-      updatedAt: new Date
-    },{
-      firstName: 'Elon',
-      lastName: 'Musk',
-      email: 'e_musk@test.com',
-      // Important: Password not encrypted yet! 
-      password: '$2b$10$oA9KaOX81D.szm.wG0zPo.QsFIDj0PiYYRwBbGDsL285YZbavAQSK',
+      firstName: 'Admin',
+      lastName: 'Test User',
+      email: 'admin-test-user@test.com',
+      password: '$2a$12$pnRk4pz/CjzV10l.p0RkoujnS1Z104wq3ILF8dKY.SioVC0f1qnzy', //TestPassword123!
       roleId: 2,
-      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
-      createdAt: new Date,
-      updatedAt: new Date
-    },{
-      firstName: 'Steeve',
-      lastName: 'Jobs',
-      email: 'steeve_j@test.com',
-      // Important: Password not encrypted yet! 
-      password: '$2b$10$oA9KaOX81D.szm.wG0zPo.QsFIDj0PiYYRwBbGDsL285YZbavAQSK',
-      roleId: 2,
-      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
-      createdAt: new Date,
-      updatedAt: new Date
-    },{
-      firstName: 'Mark',
-      lastName: 'Zuckerberg',
-      email: 'facebook@facebook.com',
-      // Important: Password not encrypted yet! 
-      password: '$2b$10$oA9KaOX81D.szm.wG0zPo.QsFIDj0PiYYRwBbGDsL285YZbavAQSK',
-      roleId: 2,
-      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
-      createdAt: new Date,
-      updatedAt: new Date
-    },{
-      firstName: 'Satoshi',
-      lastName: 'Naka',
-      email: 'btc@test.com',
-      // Important: Password not encrypted yet! 
-      password: '$2b$10$oA9KaOX81D.szm.wG0zPo.QsFIDj0PiYYRwBbGDsL285YZbavAQSK',
-      roleId: 2,
-      image: 'https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png',
+      image: 'https://i.pinimg.com/280x280_RS/2e/45/66/2e4566fd829bcf9eb11ccdb5f252b02f.jpg',
       createdAt: new Date,
       updatedAt: new Date
     }], {});
